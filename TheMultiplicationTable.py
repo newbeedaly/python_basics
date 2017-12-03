195680456a1f708742fd52c06ffcbeb9803e4ddf
HelloWorld <30494444+DicWill@users.noreply.github.com>
2017-12-03 22:56:29 +0800

    九九乘法表
    python语言编写一个简单的九九乘法表

---
+++ TheMultiplicationTable.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 _*-
+# Filename : The multipication table.py
+
+# 九九乘法表
+for i in range(1, 10):
+        for j in range(1, i+1):
+            print('{}x{}={}\t'.format(i, j, i*j), end='')
+        print()
